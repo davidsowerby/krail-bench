f8d11857bab8651b29e1c0b2b3a803ab05f45284
David Sowerby <david.sowerby@virgin.net>
2015-03-13 19:05:48 +0000

    See davidsowerby/krail#346 ViewChangeListeners replaced by Event Bus

--- src/main/java/uk/q3c/krail/testbench/page/object/NavTreePageObject.java
+++ src/main/java/uk/q3c/krail/testbench/page/object/NavTreePageObject.java
@@ -1,14 +1,12 @@
 /*
- * Copyright (c) 2014 David Sowerby
+ * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
- * the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.krail.testbench.page.object;
@@ -121,7 +119,7 @@ public class NavTreePageObject extends PageObject {
             }
         }
         if (index < 0) {
-            throw new TreePathException("Segment " + segment + "not found");
+            throw new TreePathException("Segment " + segment + " not found");
         }
         nodeInfo.index = index;
         nodeInfo.nodeElement = nodeElements.get(index);
