40a546dcc6a49e091d791a3de3accd90cb0a75c0
David Sowerby <david.sowerby@virgin.net>
2015-07-12 14:51:40 +0100

    SubPagePanelPageObject revised
    The previous method for returning the visible sub-pages fails.  Cause is not certian, but could be the change of theme - the old method relied on the getText() method of the panel.
    
     Revised to actually look for the navigation buttons

--- src/main/java/uk/q3c/krail/testbench/page/object/SubPagePanelPageObject.java
+++ src/main/java/uk/q3c/krail/testbench/page/object/SubPagePanelPageObject.java
@@ -1,25 +1,26 @@
 /*
- * Copyright (c) 2014 David Sowerby
+ * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
- * the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.krail.testbench.page.object;
 
+import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.ButtonElement;
 import com.vaadin.testbench.elements.HorizontalLayoutElement;
+import org.openqa.selenium.WebElement;
 import uk.q3c.krail.core.view.component.DefaultSubPagePanel;
 import uk.q3c.krail.core.view.component.NavigationButton;
 import uk.q3c.krail.testbench.KrailTestBenchTestCase;
+import uk.q3c.util.ID;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,10 +39,19 @@ public class SubPagePanelPageObject extends PageObject {
 
     public List<String> buttonLabels() {
         HorizontalLayoutElement panelElement = subPagePanel();
-        parentCase.pause(500);
-        String[] labelSet = panelElement.getText()
-                                        .split("\\\n");
-        List<String> labels = Arrays.asList(labelSet);
+        parentCase.pause(500);  // give the panel time to build
+        List<String> labels = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            try {
+                String id = ID.getIdc(Optional.of(i), DefaultSubPagePanel.class, NavigationButton.class);
+                WebElement element = panelElement.findElement(By.id(id));
+                labels.add(element.getText());
+            } catch (Exception e) {
+                break;
+            }
+        }
+
+
         return labels;
     }
 
